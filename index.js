6b85b75d266dffdd38a77623057686c8b954d214
Dan Garden <dangarden@gmail.com>
2019-12-19 11:43:43 +1000

    Changed routes to /api/

--- index.js
+++ index.js
@@ -7,22 +7,22 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
-app.get("/get-lyrics/:url", async (req, res) => {
+app.get("/api/get-lyrics/:url", async (req, res) => {
     const lyrics = await genius.getLyrics(req.params.url);
     res.json({lyrics: lyrics || false});
 });
 
-app.get("/search-songs/:query", async (req, res) => {
+app.get("/api/search-songs/:query", async (req, res) => {
     const results = await genius.searchSongs(req.params.query);
     res.json({results: results || []});
 });
 
-app.get("/search-lyrics/:query", async (req, res) => {
+app.get("/api/search-lyrics/:query", async (req, res) => {
     const lyrics = await genius.searchLyrics(req.params.query);
     res.json({lyrics: lyrics || false});
 });
 
-app.get("/ping", async (req, res) => {
+app.get("/api/ping", async (req, res) => {
     res.send(true);
 });
 
